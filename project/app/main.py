9f10bf15191c8612a53f9971e0377f5429180e52
Brian Keane <lonesomewhistle@gmail.com>
2021-07-09 16:55:26 -0500

    restful routes

--- project/app/main.py
+++ project/app/main.py
@@ -1,27 +1,43 @@
+import logging
+
 import os
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
 
-from app.config import get_settings, Settings
+from app.api import ping, summaries
+from app.db import init_db
+
+log = logging.getLogger("uvicorn")
 
 
-app = FastAPI()
+def create_application() -> FastAPI:
+    application = FastAPI()
+
+    register_tortoise(
+        application,
+        db_url=os.environ.get("DATABASE_URL"),
+        modules={"models": ["app.models.tortoise"]},
+        generate_schemas=False,
+        add_exception_handlers=True,
+    )
+
+    application.include_router(ping.router)
+    application.include_router(
+        summaries.router, prefix="/summaries", tags=["summaries"]
+    )
+    return application
 
 
-register_tortoise(
-    app,
-    db_url=os.environ.get("DATABASE_URL"),
-    modules={"models": ["app.models.tortoise"]},
-    generate_schemas=False,
-    add_exception_handlers=True,
-)
+app = create_application()
 
 
-@app.get("/ping")
-async def pong(settings: Settings = Depends(get_settings)):
-    return {
-        "ping": "pong!",
-        "environment": settings.environment,
-        "testing": settings.testing,
-    }
+@app.on_event("startup")
+async def startup_event():
+    log.info("Starting up...")
+    init_db(app)
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    log.info("Shutting down...")
